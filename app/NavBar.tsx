3f67fc0c0795b394513af6313e8246a79cc87083
Marek Sommer <marek.sommer.1@gmail.com>
2023-11-21 18:57:34 +0100

    Style active link in nvaBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { GoBug } from "react-icons/go";
+import classnames from "classnames";
 
 function NavBar() {
+  const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,7 +22,11 @@ function NavBar() {
             <li key={href}>
               <Link
                 href={href}
-                className="text-zinc-500 hover:text-zinc-800 transition-colors"
+                className={classnames({
+                  "text-zinc-900": href === currentPath,
+                  "text-zinc-500": href !== currentPath,
+                  "hover:text-zinc-800 transition-colors": true,
+                })}
               >
                 {label}
               </Link>
