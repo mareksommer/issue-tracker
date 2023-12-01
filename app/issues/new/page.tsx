699cd30f5c0d3f3eb09f077f9c5eca361ed8cdb3
Marek Sommer <marek.sommer.1@gmail.com>
2023-12-01 08:51:17 +0100

    Add form validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,27 +1,35 @@
 "use client";
 import axios from "axios";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "../../validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 function NewIssuePage() {
   const router = useRouter();
   const [error, setError] = useState("");
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+
   const submitForm = async (data: IssueForm) => {
     try {
       await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
-      setError('unexpected error occured');
+      setError("Unexpected error occured");
     }
   };
   return (
@@ -38,6 +46,7 @@ function NewIssuePage() {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
         <Controller
           name="description"
           control={control}
@@ -45,6 +54,9 @@ function NewIssuePage() {
             <SimpleMDE {...field} placeholder="description" />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">{errors.description.message}</Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
