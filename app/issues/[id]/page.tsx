9e5f509fe0029b9b2feec7098bd8e4259694db70
Marek Sommer <marek.sommer.1@gmail.com>
2024-01-03 11:19:39 +0100

    Add markdown in detail paeg

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,6 +2,7 @@ import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -20,7 +21,9 @@ async function IssueDetailPage({ params }: Props) {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>{issue.description}</Card>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </div>
   );
 }
