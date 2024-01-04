c2c2579f88fa96824799f8e355bdb22879e46219
Marek Sommer <marek.sommer.1@gmail.com>
2024-01-04 17:02:07 +0100

    Seup loading

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,24 @@
+import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 function LoadingIssueDetailPage() {
-  return <div>loading...</div>;
+  return (
+    <Box>
+      <Heading>
+        <Skeleton className="max-w-xl"/>
+      </Heading>
+      <Flex gap="3" my="2">
+        <Skeleton width="5rem" />
+        <Text>
+          <Skeleton width="8rem" />
+        </Text>
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 }
 
 export default LoadingIssueDetailPage;
