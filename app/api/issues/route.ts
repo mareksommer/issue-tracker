3ee3f191d7568afbca739af221d3767983905c3b
Marek Sommer <marek.sommer.1@gmail.com>
2024-01-09 12:45:26 +0100

    Secure app

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,10 +1,16 @@
 import Server from "next/server";
 import prisma from "@/prisma/client";
 import { issueSchema } from "../../validationSchemas";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: Server.NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session)
+    return Server.NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
   const body = await request.json();
-  
+
   const validation = issueSchema.safeParse(body);
   if (!validation.success)
     return Server.NextResponse.json(validation.error.format(), { status: 400 });
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,8 @@ import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./edit/DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 interface Props {
   params: { id: string };
 }
@@ -14,17 +16,21 @@ async function IssueDetailPage({ params }: Props) {
   });
 
   if (!issue) notFound();
+
+  const session = await getServerSession(authOptions);
   return (
     <Grid columns={{ initial: "1", sm: "5" }} gap="5">
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction="column" gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 }
