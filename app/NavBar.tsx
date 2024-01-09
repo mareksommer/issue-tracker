180773ff1f62421bb2aa081850e5e232b222561d
Marek Sommer <marek.sommer.1@gmail.com>
2024-01-09 12:24:53 +0100

    Refactor NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -15,13 +15,6 @@ import {
 } from "@radix-ui/themes";
 
 function NavBar() {
-  const currentPath = usePathname();
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
-  const { status, data: session } = useSession();
-
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
@@ -30,52 +23,9 @@ function NavBar() {
             <Link href="/">
               <GoBug />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map(({ label, href }) => {
-                return (
-                  <li key={href}>
-                    <Link
-                      href={href}
-                      className={classnames({
-                        "text-zinc-900": href === currentPath,
-                        "text-zinc-500": href !== currentPath,
-                        "hover:text-zinc-800 transition-colors": true,
-                      })}
-                    >
-                      {label}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    className="cursor-pointer"
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content align="end">
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Logout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Login</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
@@ -83,3 +33,63 @@ function NavBar() {
 }
 
 export default NavBar;
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return;
+  if (status === "unauthenticated")
+    return <Link className="nav-link" href="/api/auth/signin">Login</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            className="cursor-pointer"
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content align="end">
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Logout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <ul className="flex space-x-6">
+      {links.map(({ label, href }) => {
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              className={classnames({
+                "nav-link": true,
+                "!text-zinc-900": href === currentPath,
+              })}
+            >
+              {label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
