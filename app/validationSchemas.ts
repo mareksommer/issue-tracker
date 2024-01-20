bd399d0cd56fd78a1df67820af27be5feee700df
Marek Sommer <marek.sommer.1@gmail.com>
2024-01-21 00:46:31 +0100

    Implement API for assing issue to user

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,22 @@
 import z from "zod";
+import AssigneeSelect from "./issues/[id]/AssigneeSelect";
 
 export const issueSchema = z.object({
-  title: z.string().min(1, 'Title is required').max(255),
-  description: z.string().min(1, 'Description is required'),
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required").max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .max(65535)
+    .optional(),
+  AssignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
