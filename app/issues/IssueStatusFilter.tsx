f3abd3b415d429dd8a8eee55fcff17a61526e44a
Marek Sommer <marek.sommer.1@gmail.com>
2024-02-02 00:14:51 +0100

    Keep filter state

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const status: { label: string; value: Status | "ALL" }[] = [
@@ -14,11 +14,19 @@ const status: { label: string; value: Status | "ALL" }[] = [
 
 function IssueStatusFIlter() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+
   return (
     <Select.Root
+      defaultValue={searchParams.get('status') || 'ALL'}
       onValueChange={(status) => {
-        const query = status === "ALL" ? "" : `?status=${status}`;
-        router.push(`/issues${query}`);
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.has('orderBy')) params.append("orderBy", searchParams.get('orderBy')!);
+        if (searchParams.has('sortOrder')) params.append("sortOrder", searchParams.get('sortOrder')!);
+
+        const query = (params.size) ? `?${params.toString()}` : '';
+        router.push('/issues' + query);
       }}
     >
       <Select.Trigger placeholder="Filter by status..." />
